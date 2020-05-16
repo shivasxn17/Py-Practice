6b67e4b71ea16b246a5823183778be3dc50a1fa9
shivamsaxena17493 <shivamsaxena17@live.com>
2020-05-16 18:17:57 +0100

    cytoscape test

---
+++ dash/tree_plot_test.py
@@ -0,0 +1,69 @@
+import igraph
+from igraph import Graph, EdgeSeq
+import plotly.graph_objects as go
+import json
+
+nr_vertices = 25
+v_label = list(map(str, range(nr_vertices)))
+G = Graph.Tree(nr_vertices, 2) # 2 stands for children number
+lay = G.layout('rt')
+
+position = {k: lay[k] for k in range(nr_vertices)}
+Y = [lay[k][1] for k in range(nr_vertices)]
+M = max(Y)
+
+es = EdgeSeq(G) # sequence of edges
+E = [e.tuple for e in G.es] # list of edges
+
+L = len(position)
+Xn = [position[k][0] for k in range(L)]
+Yn = [2*M-position[k][1] for k in range(L)]
+Xe = []
+Ye = []
+for edge in E:
+    Xe+=[position[edge[0]][0],position[edge[1]][0], None]
+    Ye+=[2*M-position[edge[0]][1],2*M-position[edge[1]][1], None]
+
+labels = v_label
+
+
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=Xe,
+                   y=Ye,
+                   mode='lines',
+                   line=dict(color='rgb(210,210,210)', width=1),
+                   hoverinfo='none'
+                   ))
+fig.add_trace(go.Scatter(x=Xn,
+                  y=Yn,
+                  mode='markers',
+                  name='bla',
+                  marker=dict(symbol='circle-dot',
+                                size=18,
+                                color='#6175c1',    #'#DB4551',
+                                line=dict(color='rgb(50,50,50)', width=1)
+                                ),
+                  text=labels,
+                  hoverinfo='text',
+                  opacity=0.8
+                  ))
+
+axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            )
+
+fig.update_layout(title= 'Tree with Reingold-Tilford Layout',
+              # annotations=make_annotations(position, v_label),
+              font_size=12,
+              showlegend=False,
+              xaxis=axis,
+              yaxis=axis,
+              margin=dict(l=40, r=40, b=85, t=100),
+              hovermode='closest',
+              plot_bgcolor='rgb(248,248,248)'
+              )
+
+fig.show()
